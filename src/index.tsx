dfd558f7206efa1d6d2b4c5a8204f4a27a434122
Erik Golinelli <erik@codekraft.it>
2023-01-26 20:58:07 +0100

    adds some basic block attributes fix for block attributes definition (removes the custom one)

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
-import { registerBlockType } from '@wordpress/blocks';
-/** add styles to bundle */
+import { BlockAttributes, registerBlockType } from '@wordpress/blocks';
+/* adds styles to bundle */
 import './style/style-admin.scss';
 
-/** the edit function */
+/* the edit function */
 import Edit from './edit';
 
-/** the save function */
+/* the save function */
 import Save from './save';
 
 /* Block settings */
 import blockConfig from '../block.json';
+const jsonData = blockConfig as BlockAttributes;
 
 /** Registering the block with the name of the block and the attributes of the block. */
 registerBlockType( jsonData.name, {
@@ -22,6 +23,16 @@ registerBlockType( jsonData.name, {
 	 * @see ./save.js
 	 */
 	save: Save,
+  icon: 'universal-access-alt',
+	supports: {
+		align: true,
+		className: true,
+		spacing: {
+			margin: true, // Enable margin UI control.
+			padding: true, // Enable padding UI control.
+			blockGap: true, // Enables block spacing UI control.
+		},
+	},
 	attributes: {
 		message: {
 			type: 'string',
