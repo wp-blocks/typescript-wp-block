7f8a6be5034edb70ce8de03531673c9048b7d881
Erik Golinelli <erik@codekraft.it>
2023-11-26 18:27:14 +0100

    removes babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,10 @@
-const path = require( 'path' );
+const path = require('path');
 
-const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 
 module.exports = {
 	...defaultConfig,
 	entry: {
-		boilerplate: path.resolve( process.cwd(), `src/index.tsx` ),
-	},
-	devtool: 'inline-source-map',
-	module: {
-		rules: [
-			{
-				test: /\.[tjmc]sx?$/,
-				use: [ 'babel-loader' ],
-				exclude: /node_modules/,
-			},
-		],
-		...defaultConfig.module,
+		boilerplate: path.resolve(process.cwd(), `src/index.tsx`),
 	},
 };
